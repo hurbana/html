43687fe3063f458d82915cf6a3f87ceebb296017
Habtamu <86288958+hurbana@users.noreply.github.com>
2024-08-12 13:39:37 +0000

    commit

---
+++ nathan/add.js
@@ -0,0 +1,7 @@
+function add(x, y){
+    return x + y;
+}
+let s1 = add(7,10);
+console.log("s1 = " + s1);
+let s2 = add(99090000082,12345678910);
+console.log("s2 = " + s2);
---
+++ nathan/main.js
@@ -0,0 +1,6 @@
+let z;
+z = 5;
+let x = 10;
+let y = 11;
+let sum = y + x + z;
+console.log("sum " + sum);
