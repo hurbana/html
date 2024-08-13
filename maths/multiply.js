3be5bdb1dbd58babd122ae1dd4f7e47013b35fd1
Habtamu <86288958+hurbana@users.noreply.github.com>
2024-08-13 15:05:29 +0000

    added maths

---
+++ maths/multiply.js
@@ -0,0 +1,6 @@
+function multiply(a, b){
+    return a * b;    
+}
+
+let multiply2number = multiply(100, 100);
+ console.log("multiply = " + multiply2number);
---
+++ maths/sum.js
@@ -0,0 +1,8 @@
+function sum(x, y){
+    let s1 = x + y;
+    return s1;
+}
+
+let add2number = sum(1000000,200);
+console.log("sum = " + add2number);
+
